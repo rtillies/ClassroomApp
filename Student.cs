166ab1f6c196be0513518c6d6d2996fa3d696514
Richard T <richard@turing.edu>
2023-05-31 15:47:03 -0400

    Object classes created

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+        public List<Course> CourseList { get; set; }
+    }
+}
